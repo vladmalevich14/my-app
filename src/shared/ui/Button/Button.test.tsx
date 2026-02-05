ac15260c69c7e75e2dfa0d391632bbdd70528ac4
vladmalevich14 <vladmalevich1431@gmail.com>
2026-02-05 18:29:38 +0300

    Initial commit

---
+++ src/shared/ui/Button/Button.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react';
+import { Button } from 'shared/ui/Button/Button';
+import { ThemeButton } from './Button';
+
+describe('Button', () => {
+    test('Test render', () => {
+        render(<Button>TEST</Button>);
+        expect(screen.getByText('TEST'))
+            .toBeInTheDocument();
+    });
+
+    test('Test clear class', () => {
+        render(<Button theme={ThemeButton.CLEAR}>TEST</Button>);
+        expect(screen.getByText('TEST'))
+            .toHaveClass('clear');
+        screen.debug();
+    });
+});
